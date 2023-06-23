5f9493e3ee4eed4f045fccdd7793eb71bf1e2b36
isaquexavier33 <isaquexavier57@gmail.com>
2023-06-23 11:52:20 -0300

    Áudio e animações

--- game/js/game.js
+++ game/js/game.js
@@ -1,5 +1,8 @@
 console.log("Flappy Bird");
 
+let frames = 0;
+const soundHit = new Audio("./game/assets/audio/hit.wav");
+
 const sprites = new Image();
 sprites.src = "./game/assets/img/sprites.png";
 
@@ -44,70 +47,124 @@ const background = {
   },
 };
 
-const floor = {
-  spriteX: 0,
-  spriteY: 611,
-  width: 224,
-  height: 112,
-  x: 0,
-  y: canvas.height - 112,
+function createFloor() {
+  const floor = {
+    spriteX: 0,
+    spriteY: 611,
+    width: 224,
+    height: 112,
+    x: 0,
+    y: canvas.height - 112,
+    
+    update() {
+      const floorMovement = 1;
 
-  draw() {
-    context.drawImage(
-      sprites,
-      floor.spriteX,
-      floor.spriteY,
-      floor.width,
-      floor.height,
-      floor.x,
-      floor.y,
-      floor.width,
-      floor.height
-    );
+      const repeatIn = floor.width / 2;
+      const movement = floor.x -= floorMovement;
+      floor.x = movement % repeatIn;
+    },
+    draw() {
+      context.drawImage(
+        sprites,
+        floor.spriteX,
+        floor.spriteY,
+        floor.width,
+        floor.height,
+        floor.x,
+        floor.y,
+        floor.width,
+        floor.height
+      );
+  
+      context.drawImage(
+        sprites,
+        floor.spriteX,
+        floor.spriteY,
+        floor.width,
+        floor.height,
+        floor.x + floor.width,
+        floor.y, // Deslocado à direita do primeiro chão
+        floor.width,
+        floor.height
+      );
+    },
 
-    context.drawImage(
-      sprites,
-      floor.spriteX,
-      floor.spriteY,
-      floor.width,
-      floor.height,
-      floor.x + floor.width,
-      floor.y, // Deslocado à direita do primeiro chão
-      floor.width,
-      floor.height
-    );
-  },
-};
+  };
+  
+  return floor;
+}
 
-const flappyBird = {
-  spriteX: 0,
-  spriteY: 0,
-  width: 34,
-  height: 24,
-  x: 10,
-  y: 50,
-  gravity: 0.25,
-  velocity: 0,
 
-  update() {
-    flappyBird.velocity += flappyBird.gravity;
-    flappyBird.y += flappyBird.velocity;
-  },
+function createFlappyBird() {
+  const flappyBird = {
+    spriteX: 0,
+    spriteY: 0,
+    width: 34,
+    height: 24,
+    x: 10,
+    y: 50,
+    gravity: 0.25,
+    velocity: 0,
+    jumpSize: 4.6,
+    movements: [
+      { spriteX: 0, spriteY: 0 }, // asa pra cima
+      { spriteX: 0, spriteY: 26, }, // asa no meio
+      { spriteX: 0, spriteY: 52, } // asa pra baixo
+    ],
+    currentFrame: 0,
 
-  draw() {
-    context.drawImage(
-      sprites, // Image
-      flappyBird.spriteX,
-      flappyBird.spriteY, // PosX e Y do Sprite no spriteSheet
-      flappyBird.width,
-      flappyBird.height, // SpriteWidth, SpriteHeight
-      flappyBird.x,
-      flappyBird.y, // PosX e PosY no canvas
-      flappyBird.width,
-      flappyBird.height // width e height no canvas
-    );
-  },
-};
+    updateCurrentFrame() {
+      const framesInterval = 10;
+      const passedTheInterval = frames % framesInterval === 0;
+
+      if (passedTheInterval) {
+        const baseOfIncrement = 1;
+        const increment = baseOfIncrement + flappyBird.currentFrame;
+        const baseOfRepetition = flappyBird.movements.length;
+        flappyBird.currentFrame = increment % baseOfRepetition;
+      }
+    },
+
+    update() {
+      if (hasCollision(flappyBird, globals.floor)) {
+        soundHit.play();
+
+        setTimeout(() => {
+          changeScreen(Screens.START);
+        }, 0.5 * 1000);
+
+        return;
+      }
+
+      flappyBird.velocity += flappyBird.gravity;
+      flappyBird.y += flappyBird.velocity;
+    },
+
+    draw() {
+      flappyBird.updateCurrentFrame();
+      const { spriteX, spriteY } = flappyBird.movements[flappyBird.currentFrame];
+
+      context.drawImage(
+        sprites, // Image
+        spriteX,
+        spriteY, // PosX e Y do Sprite no spriteSheet
+        flappyBird.width,
+        flappyBird.height, // SpriteWidth, SpriteHeight
+        flappyBird.x,
+        flappyBird.y, // PosX e PosY no canvas
+        flappyBird.width,
+        flappyBird.height // width e height no canvas
+      );
+    },
+
+    jump() {
+      flappyBird.currentFrame = 2;
+      flappyBird.velocity = -flappyBird.jumpSize;
+    },
+  };
+
+  return flappyBird;
+}
 
 const messageGetReady = {
   spriteX: 134,
@@ -135,34 +192,55 @@ const messageGetReady = {
 //
 // Telas
 //
+const globals = {};
 let activeScreen = {};
 
 function changeScreen(newScreen) {
   activeScreen = newScreen;
+
+  if (activeScreen.init) activeScreen.init();
+}
+
+function hasCollision(flappyBird, floor) {
+  const flappyBirdY = flappyBird.y + flappyBird.height;
+  const floorY = floor.y;
+
+  if (flappyBirdY >= floorY) return true;
+  return false;
 }
 
 const Screens = {
-  start: {
+  START: {
+    init() {
+      globals.flappyBird = createFlappyBird();
+      globals.floor = createFloor();
+    },
+
     draw() {
       messageGetReady.draw();
     },
 
     click() {
-      changeScreen(Screens.game);
+      changeScreen(Screens.GAME);
     },
 
-    update() {},
+    update() {}
   },
-  game: {
+  GAME: {
     draw() {
       // A ordem em que você desenha importa na sobreposição
       background.draw();
-      floor.draw();
-      flappyBird.draw();
+      globals.floor.draw();
+      globals.flappyBird.draw();
+    },
+
+    click() {
+      globals.flappyBird.jump();
     },
 
     update() {
-      flappyBird.update();
+      globals.flappyBird.update();
+      globals.floor.update();
     },
   },
 };
@@ -171,12 +249,18 @@ function loop() {
   activeScreen.draw();
   activeScreen.update();
 
+  frames++;
+
   requestAnimationFrame(loop);
 }
+
+window.addEventListener("keydown", (event) => {
+  if (event.code == "ArrowUp") if (activeScreen.click) activeScreen.click();
+});
 
 window.addEventListener("click", () => {
   if (activeScreen.click) activeScreen.click();
 });
 
-changeScreen(Screens.start);
+changeScreen(Screens.START);
 loop();
