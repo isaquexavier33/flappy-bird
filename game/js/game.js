7730b4f43f7baa70a3f2454a422debb12b79aabc
isaquexavier33 <isaquexavier57@gmail.com>
2023-06-24 11:40:43 -0300

    Obstáculos adicionados

--- game/js/game.js
+++ game/js/game.js
@@ -55,12 +55,12 @@ function createFloor() {
     height: 112,
     x: 0,
     y: canvas.height - 112,
-    
+
     update() {
       const floorMovement = 1;
 
       const repeatIn = floor.width / 2;
-      const movement = floor.x -= floorMovement;
+      const movement = (floor.x -= floorMovement);
       floor.x = movement % repeatIn;
     },
     draw() {
@@ -75,7 +75,7 @@ function createFloor() {
         floor.width,
         floor.height
       );
-  
+
       context.drawImage(
         sprites,
         floor.spriteX,
@@ -88,12 +88,10 @@ function createFloor() {
         floor.height
       );
     },
-
   };
-  
+
   return floor;
 }
-
 
 function createFlappyBird() {
   const flappyBird = {
@@ -108,8 +106,8 @@ function createFlappyBird() {
     jumpSize: 4.6,
     movements: [
       { spriteX: 0, spriteY: 0 }, // asa pra cima
-      { spriteX: 0, spriteY: 26, }, // asa no meio
-      { spriteX: 0, spriteY: 52, } // asa pra baixo
+      { spriteX: 0, spriteY: 26 }, // asa no meio
+      { spriteX: 0, spriteY: 52 }, // asa pra baixo
     ],
     currentFrame: 0,
 
@@ -142,7 +140,8 @@ function createFlappyBird() {
 
     draw() {
       flappyBird.updateCurrentFrame();
-      const { spriteX, spriteY } = flappyBird.movements[flappyBird.currentFrame];
+      const { spriteX, spriteY } =
+        flappyBird.movements[flappyBird.currentFrame];
 
       context.drawImage(
         sprites, // Image
@@ -164,6 +163,113 @@ function createFlappyBird() {
   };
 
   return flappyBird;
+}
+
+function createPipes() {
+  const pipes = {
+    width: 52,
+    height: 400,
+    floor: {
+      spriteX: 0,
+      spriteY: 169,
+    },
+    sky: {
+      spriteX: 52,
+      spriteY: 169,
+    },
+    spaces: 80,
+
+    draw() {
+      pipes.pairsOfPipes.forEach((pair) => {
+        const yRandom = pair.y;
+        const spacingBetweenPipes = 90;
+
+        // Pipe of sky
+        const pipeOfSkyX = pair.x;
+        const pipeOfSkyY = yRandom;
+
+        context.drawImage(
+          sprites,
+          pipes.sky.spriteX,
+          pipes.sky.spriteY,
+          pipes.width,
+          pipes.height,
+          pipeOfSkyX,
+          pipeOfSkyY,
+          pipes.width,
+          pipes.height
+        );
+
+        // Pipe of floor
+        const pipeOfFloorX = pair.x;
+        const pipeOfFloorY = pipes.height + spacingBetweenPipes + yRandom;
+
+        context.drawImage(
+          sprites,
+          pipes.floor.spriteX,
+          pipes.floor.spriteY,
+          pipes.width,
+          pipes.height,
+          pipeOfFloorX,
+          pipeOfFloorY,
+          pipes.width,
+          pipes.height
+          );
+
+        pair.pipeOfSky = {
+          x: pipeOfFloorX,
+          y: pipes.height + pipeOfSkyY
+        }
+          
+        pair.pipeOfFloor = {
+          x: pipeOfSkyX,
+          y: pipeOfFloorY
+        };
+      });
+    },
+
+    hasCollisionWithFlappyBird(pair) {
+      const headOfFlappyBird = globals.flappyBird.y;
+      const footOfFlappyBird = globals.flappyBird.y + globals.flappyBird.height;
+
+      if (globals.flappyBird.x >= pair.x) {
+        if (headOfFlappyBird <= pair.pipeOfSky.y) {
+          return true;
+        }
+
+        if (footOfFlappyBird >= pair.pipeOfFloor.y) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+
+    pairsOfPipes: [],
+    update() {
+      const passed100Frames = frames % 100 === 0;
+      if (passed100Frames) {
+        pipes.pairsOfPipes.push(
+          {
+            x: canvas.width,
+            y: -200 * (Math.random() + 1),
+          }
+        );
+      }
+
+      pipes.pairsOfPipes.forEach((pair) => {
+        pair.x -= 2;
+
+        if (pipes.hasCollisionWithFlappyBird(pair)) {
+          changeScreen(Screens.START);
+        }
+
+        if (pair.x + pipes.width <= 0) pipes.pairsOfPipes.shift();
+      });
+    },
+  };
+
+  return pipes;
 }
 
 const messageGetReady = {
@@ -214,9 +320,13 @@ const Screens = {
     init() {
       globals.flappyBird = createFlappyBird();
       globals.floor = createFloor();
+      globals.pipes = createPipes();
     },
 
     draw() {
+      background.draw();
+      globals.flappyBird.draw();
+      globals.floor.draw();
       messageGetReady.draw();
     },
 
@@ -224,12 +334,15 @@ const Screens = {
       changeScreen(Screens.GAME);
     },
 
-    update() {}
+    update() {
+      globals.floor.update();
+    },
   },
   GAME: {
     draw() {
       // A ordem em que você desenha importa na sobreposição
       background.draw();
+      globals.pipes.draw();
       globals.floor.draw();
       globals.flappyBird.draw();
     },
@@ -240,6 +353,7 @@ const Screens = {
 
     update() {
       globals.flappyBird.update();
+      globals.pipes.update();
       globals.floor.update();
     },
   },
