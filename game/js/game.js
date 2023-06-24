0a98cabb875ca6cedc299bef8426bcb74e6caf3d
isaquexavier33 <isaquexavier57@gmail.com>
2023-06-24 13:24:23 -0300

    Pontuação, tela de gameover e mais

--- game/js/game.js
+++ game/js/game.js
@@ -1,7 +1,20 @@
-console.log("Flappy Bird");
-
 let frames = 0;
+
+// Sounds
+
 const soundHit = new Audio("./game/assets/audio/hit.wav");
+soundHit.volume = 0.8;
+
+const soundPoint = new Audio("./game/assets/audio/point.wav");
+soundPoint.volume = 0.4;
+
+const soundFall = new Audio("./game/assets/audio/fall.wav");
+soundFall.volume = 0.4;
+
+const soundJump = new Audio("./game/assets/audio/jump.wav");
+soundJump.volume = 0.1;
+
+// Sprites
 
 const sprites = new Image();
 sprites.src = "./game/assets/img/sprites.png";
@@ -9,43 +22,47 @@ sprites.src = "./game/assets/img/sprites.png";
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-const background = {
-  spriteX: 390,
-  spriteY: 0,
-  width: 276,
-  height: 204,
-  x: 0,
-  y: canvas.height - 204,
+function createBackground() {
+  const background = {
+    spriteX: 390,
+    spriteY: 0,
+    width: 276,
+    height: 204,
+    x: 0,
+    y: canvas.height - 204,
 
-  draw() {
-    context.fillStyle = "#70c5ce";
-    context.fillRect(0, 0, canvas.width, canvas.height - 112);
+    draw() {
+      context.fillStyle = "#70c5ce";
+      context.fillRect(0, 0, canvas.width, canvas.height - 112);
 
-    context.drawImage(
-      sprites,
-      background.spriteX,
-      background.spriteY,
-      background.width,
-      background.height,
-      background.x,
-      background.y,
-      background.width,
-      background.height
-    );
+      context.drawImage(
+        sprites,
+        background.spriteX,
+        background.spriteY,
+        background.width,
+        background.height,
+        background.x,
+        background.y,
+        background.width,
+        background.height
+      );
 
-    context.drawImage(
-      sprites,
-      background.spriteX,
-      background.spriteY,
-      background.width,
-      background.height,
-      background.x + background.width,
-      background.y,
-      background.width,
-      background.height
-    );
-  },
-};
+      context.drawImage(
+        sprites,
+        background.spriteX,
+        background.spriteY,
+        background.width,
+        background.height,
+        background.x + background.width,
+        background.y,
+        background.width,
+        background.height
+      );
+    },
+  };
+
+  return background;
+}
 
 function createFloor() {
   const floor = {
@@ -125,11 +142,9 @@ function createFlappyBird() {
 
     update() {
       if (hasCollision(flappyBird, globals.floor)) {
-        soundHit.play();
+        soundFall.play();
 
-        setTimeout(() => {
-          changeScreen(Screens.START);
-        }, 0.5 * 1000);
+        changeScreen(Screens.GAME_OVER);
 
         return;
       }
@@ -157,6 +172,7 @@ function createFlappyBird() {
     },
 
     jump() {
+      soundJump.play();
       flappyBird.currentFrame = 2;
       flappyBird.velocity = -flappyBird.jumpSize;
     },
@@ -214,16 +230,16 @@ function createPipes() {
           pipeOfFloorY,
           pipes.width,
           pipes.height
-          );
+        );
 
         pair.pipeOfSky = {
           x: pipeOfFloorX,
-          y: pipes.height + pipeOfSkyY
-        }
-          
+          y: pipes.height + pipeOfSkyY,
+        };
+
         pair.pipeOfFloor = {
           x: pipeOfSkyX,
-          y: pipeOfFloorY
+          y: pipeOfFloorY,
         };
       });
     },
@@ -232,12 +248,12 @@ function createPipes() {
       const headOfFlappyBird = globals.flappyBird.y;
       const footOfFlappyBird = globals.flappyBird.y + globals.flappyBird.height;
 
-      if (globals.flappyBird.x >= pair.x) {
-        if (headOfFlappyBird <= pair.pipeOfSky.y) {
+      if (globals.flappyBird.x + globals.flappyBird.width > pair.x) {
+        if (headOfFlappyBird < pair.pipeOfSky.y) {
           return true;
         }
 
-        if (footOfFlappyBird >= pair.pipeOfFloor.y) {
+        if (footOfFlappyBird > pair.pipeOfFloor.y) {
           return true;
         }
       }
@@ -249,19 +265,18 @@ function createPipes() {
     update() {
       const passed100Frames = frames % 100 === 0;
       if (passed100Frames) {
-        pipes.pairsOfPipes.push(
-          {
-            x: canvas.width,
-            y: -200 * (Math.random() + 1),
-          }
-        );
+        pipes.pairsOfPipes.push({
+          x: canvas.width,
+          y: -200 * (Math.random() + 1),
+        });
       }
 
       pipes.pairsOfPipes.forEach((pair) => {
         pair.x -= 2;
 
         if (pipes.hasCollisionWithFlappyBird(pair)) {
-          changeScreen(Screens.START);
+          soundHit.play();
+          changeScreen(Screens.GAME_OVER);
         }
 
         if (pair.x + pipes.width <= 0) pipes.pairsOfPipes.shift();
@@ -270,6 +285,48 @@ function createPipes() {
   };
 
   return pipes;
+}
+
+function createScore() {
+  const score = {
+    pointing: 0,
+
+    draw() {
+      context.font = "35px 'VT323'";
+      context.textAlign = "right";
+      context.fillStyle = "white";
+      context.fillText(`Score: ${score.pointing}`, canvas.width - 10, 35);
+    },
+
+    update() {
+      const framesInterval = 20;
+      const passedTheInterval = frames % framesInterval === 0;
+
+      let flappyBirdX = globals.flappyBird.x;
+      let canoX;
+
+      switch (globals.pipes.pairsOfPipes.length) {
+        case 0:
+          return;
+        case 1:
+          canoX = globals.pipes.pairsOfPipes[0].x;
+          break;
+        case 2:
+          canoX = Math.min(
+            globals.pipes.pairsOfPipes[0].x,
+            globals.pipes.pairsOfPipes[1].x
+          );
+      }
+
+      if (flappyBirdX === canoX) {
+        score.pointing++;
+
+        soundPoint.play();
+      }
+    },
+  };
+
+  return score;
 }
 
 const messageGetReady = {
@@ -291,6 +348,29 @@ const messageGetReady = {
       messageGetReady.y,
       messageGetReady.width,
       messageGetReady.height
+    );
+  },
+};
+
+const messageGameOver = {
+  spriteX: 134,
+  spriteY: 153,
+  width: 226,
+  height: 200,
+  x: canvas.width / 2 - 226 / 2,
+  y: 50,
+
+  draw() {
+    context.drawImage(
+      sprites,
+      messageGameOver.spriteX,
+      messageGameOver.spriteY,
+      messageGameOver.width,
+      messageGameOver.height,
+      messageGameOver.x,
+      messageGameOver.y,
+      messageGameOver.width,
+      messageGameOver.height
     );
   },
 };
@@ -318,13 +398,14 @@ function hasCollision(flappyBird, floor) {
 const Screens = {
   START: {
     init() {
+      globals.background = createBackground();
       globals.flappyBird = createFlappyBird();
       globals.floor = createFloor();
       globals.pipes = createPipes();
     },
 
     draw() {
-      background.draw();
+      globals.background.draw();
       globals.flappyBird.draw();
       globals.floor.draw();
       messageGetReady.draw();
@@ -339,12 +420,17 @@ const Screens = {
     },
   },
   GAME: {
+    init() {
+      globals.score = createScore();
+    },
+
     draw() {
       // A ordem em que você desenha importa na sobreposição
-      background.draw();
+      globals.background.draw();
       globals.pipes.draw();
       globals.floor.draw();
       globals.flappyBird.draw();
+      globals.score.draw();
     },
 
     click() {
@@ -355,6 +441,18 @@ const Screens = {
       globals.flappyBird.update();
       globals.pipes.update();
       globals.floor.update();
+      globals.score.update();
+    },
+  },
+  GAME_OVER: {
+    draw() {
+      messageGameOver.draw();
+    },
+
+    update() {},
+
+    click() {
+      changeScreen(Screens.START);
     },
   },
 };
@@ -369,7 +467,7 @@ function loop() {
 }
 
 window.addEventListener("keydown", (event) => {
-  if (event.code == "ArrowUp") if (activeScreen.click) activeScreen.click();
+  if (event.code == "Space") if (activeScreen.click) activeScreen.click();
 });
 
 window.addEventListener("click", () => {
